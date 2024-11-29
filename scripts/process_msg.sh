38f4e82f5a8605a864b62f9a518290fd8a5ec957
Alexander Neumann <aleneum@gmail.com>
2024-11-29 18:20:36 +0100

    wont work

---
+++ scripts/process_msg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Input should be release(<image-name>): <version>
+# echo <image-name> when $2 is 1 and <version> otherwise
+if [ $2 -eq 1 ]; then
+    echo $(awk -F'[()]' '{print $2}' <<< $1)
+else
+    echo $(awk '{print $2}' <<< $1)
+fi

6647b40b4a23b8ce023bb8d854424877fdce6f71
Alexander Neumann <aleneum@gmail.com>
2024-11-29 15:20:55 +0100

    feat(gha): checkout is shallow. git diff does not work there

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -3,9 +3,8 @@
 # Rule 1: Changes must be limited to a single subfolder
 
 base_dir=""
-git fetch origin main
-echo $(git diff --name-only origin/main HEAD)
-for file in $(git diff --name-only origin/main HEAD); do
+echo $(git diff --name-only origin/${{ github.base_ref }}..${{ github.sha }})
+for file in $(git diff --name-only origin/${{ github.base_ref }}..${{ github.sha }}); do
     echo "Linter: Checking $file"
     # Check if base_dir is set
     if [ -z "$base_dir" ]; then

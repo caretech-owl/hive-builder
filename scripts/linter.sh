318182b023085f91e7f39044d4abe8f49cfe4e32
Alexander Neumann <aleneum@gmail.com>
2024-12-03 23:14:15 +0100

    fix(gha): linter base_dir was fixed

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -2,23 +2,23 @@
 
 # Rule 1: Changes must be limited to a single subfolder
 
-base_dir="images/gerd"
-# for file in $(git diff --name-only HEAD~1); do
-#     # Check if base_dir is set
-#     if [ -z "$base_dir" ]; then
-#         base_dir=$(awk -F/ 'NF >= 2 { print $1 "/" $2 } NF == 1 { print $1 }' <<< $file)
-#         if [[ $base_dir != images/* ]]; then
-#             echo "Linter: ❌ $file appears to be outside of an image directory. Changes must be limited to a single image. You might need to rebase your PR."
-#             exit 1
-#         fi
-#     else
-#         candidate_dir=$(awk -F/ 'NF >= 2 { print $1 "/" $2 } NF == 1 { print $1 }' <<< $file)
-#         if [ "$base_dir" != "$candidate_dir" ]; then
-#             echo "Linter: ❌ Found changes in $base_dir and $candidate_dir. Changes must be limited to a single image. You might need to rebase your PR."
-#             exit 1
-#         fi
-#     fi
-# done
+base_dir=""
+for file in $(git diff --name-only HEAD~1); do
+    # Check if base_dir is set
+    if [ -z "$base_dir" ]; then
+        base_dir=$(awk -F/ 'NF >= 2 { print $1 "/" $2 } NF == 1 { print $1 }' <<< $file)
+        if [[ $base_dir != images/* ]]; then
+            echo "Linter: ❌ $file appears to be outside of an image directory. Changes must be limited to a single image. You might need to rebase your PR."
+            exit 1
+        fi
+    else
+        candidate_dir=$(awk -F/ 'NF >= 2 { print $1 "/" $2 } NF == 1 { print $1 }' <<< $file)
+        if [ "$base_dir" != "$candidate_dir" ]; then
+            echo "Linter: ❌ Found changes in $base_dir and $candidate_dir. Changes must be limited to a single image. You might need to rebase your PR."
+            exit 1
+        fi
+    fi
+done
 
 if [ -z "$base_dir" ]; then
     echo "Linter: ❌ No files found in the diff. Please make sure you are comparing against the correct branch."
